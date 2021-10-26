fd8bfe53bfdc8fbe18b3f2faefeed1f7619f9dff
Matthew Tang <matthew.tang@connect.hku.hk>
2021-10-26 19:53:53 +0800

    add mockContracts

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,8 +1,31 @@
-from brownie import accounts, network, config
+from brownie import (
+    accounts,
+    network,
+    config,
+    MockV3Aggregator,
+    MockDAI,
+    MockWETH,
+    LinkToken,
+)
+import eth_utils
 
-LOCAL_FORK_MAINNET = ["mainnet-fork"]
+INITIAL_PRICE_FEED_VALUE = 2000 * 10 ** 18
+DECIMALS = 18
 
-LOCAL_BLOCKCHAIN_ENVIRONMENT = LOCAL_FORK_MAINNET + ["development"]
+NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache"]
+
+LOCAL_BLOCKCHAIN_ENVIRONMENT = NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS + [
+    "mainnet-fork",
+    "binance-fork",
+    "matic-fork",
+]
+
+contract_to_mock = {
+    "eth_usd_price_feed": MockV3Aggregator,
+    "dai_usd_price_feed": MockV3Aggregator,
+    "fau_token": MockDAI,
+    "weth_token": MockWETH,
+}
 
 
 def get_account(index=None, id=None):
@@ -13,3 +36,64 @@ def get_account(index=None, id=None):
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENT:
         return accounts[0]
     return accounts.add(config["wallets"]["from_key"])
+
+
+def get_contract(contrace_name):
+    """
+    Args:
+        contract_name(string): name refered to in the brownie config and 'contract_to_mock' variable
+    Returns:
+        brownie.network.contract.ProjectContract: The most recently deployed contract of the type
+        specified by the dictionary.  This cold either be a mock or the 'real' contract on a live
+        network
+    """
+    contract_type = contract_to_mock[contrace_name]
+    if network.show_active() in NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+    else:
+        try:
+            contract_address = config["networks"][network.show_active()][contrace_name]
+            contract = Contract.from_abi(
+                contract_type._name, contract_address, contract_type._abi
+            )
+        except KeyError:
+            print(
+                f"{network.show_active()} address not found, perhaps you should add it to the config or deploy mocks?"
+            )
+            print(
+                f"brownie run scripts/deploy_mocks.py --network {network.show_active()}"
+            )
+    return contract
+
+
+def get_verify_status():
+    verify = config["networks"][network.show_active()]["verify"]
+    return verify
+
+
+def deploy_mocks(decimals=DECIMALS, initial_price_feed_value=INITIAL_PRICE_FEED_VALUE):
+    print(f"The active network is {network.show_active()}")
+    print(f"Deploying mocks for {network.show_active()}")
+    account = get_account()
+    print("Deploying Mock Link Token")
+    link_token = LinkToken.deploy({"from": account})
+    print(f"deployed to {link_token.address}")
+    print("Deploying Mock price feed ")
+    mock_price_feed = MockV3Aggregator.deploy(
+        decimals, initial_price_feed_value, {"from": account}
+    )
+    print(f"deployed to {mock_price_feed.address}")
+    print("Deploying Mock Dai Token")
+    dai_token = MockDAI.deploy({"from": account})
+    print(f"deployed to {dai_token.address}")
+    print("Deploying Mock weth Token")
+    weth_token = MockWETH.deploy({"from": account})
+    print(f"deployed to {weth_token.address}")
+
+
+def encode_function_data(initialiser=None, *args):
+    if len(args) == 0 or not initialiser:
+        return eth_utils.to_bytes(hexstr="0x")
+    return initialiser.encode_input(*args)
